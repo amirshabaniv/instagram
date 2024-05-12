a9f2d3cba0666f46254fffe620428c1bff56573f
Amir Shabani <129328075+amirshabaniv@users.noreply.github.com>
2024-05-12 18:10:22 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,22 +76,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'instagram',
         'USER': 'postgres',
-        'PASSWORD': 'amir4014',
+        'PASSWORD': 'pass',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -159,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'amirshabaniv@gmail.com'
-EMAIL_HOST_PASSWORD = 'ysovtddfvhenhuxs'
+EMAIL_HOST_PASSWORD = 'pass'
 EMAIL_USE_TLS = True
 
 
